cac14b18b14ad441bcab08b2143a52b697e31905
linuxgame <linxu0486@gmail.com>
2023-04-06 05:58:11 +0900

    first project test commit

--- test.dart
+++ test.dart
@@ -1,33 +1,27 @@
 void main() {
-
-  
   Idol ssh = Idol(
     name: '성호',
     group: 'linuxgame',
   );
 
-  
   ssh.sayName();
   print(ssh.name);
   print(ssh.group);
-  
 
-  // 여기서 두번째 수정을 좀 해봤어.]
+  // 여기서 두번째 수정을 좀 해봤어.
   // 여기서 세번째 수정을 좀 해봤어.
-  
 }
-
 
 class Idol {
   String name;
   String group;
-  
-  Idol ({
+
+  Idol({
     String name = '',
     String group = '',
   })  : this.name = name,
         this.group = group;
-  
+
   void sayName() {
     print('제 이름은 ${this.name}입니다.');
   }
